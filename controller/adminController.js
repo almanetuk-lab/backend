f4daef25eb5784b11e5035f37383772c63146fcb
almanetuk-lab <almanetuk@gmail.com>
2025-10-18 12:30:51 +0530

    admin api update

--- controller/adminController.js
+++ controller/adminController.js
@@ -1,44 +1,44 @@
-// import bcrypt from "bcrypt";
-// import jwt from "jsonwebtoken";
-// import dotenv from "dotenv";
-// import { pool } from "../config/db.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { pool } from "../config/db.js";
 
-// dotenv.config();
+dotenv.config();
 
-// // ---------------- Admin Login ----------------
-// export const adminLogin = async (req, res) => {
-//   try {
-//     const { email, password } = req.body;
+// ---------------- Admin Login ----------------
+export const adminLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-//     const result = await pool.query("SELECT * FROM admins WHERE email=$1", [email]);
-//     const admin = result.rows[0];
+    const result = await pool.query("SELECT * FROM admins WHERE email=$1", [email]);
+    const admin = result.rows[0];
 
-//     if (!admin) return res.status(404).json({ message: "Admin not found" });
+    if (!admin) return res.status(404).json({ message: "Admin not found" });
 
-//     const isMatch = await bcrypt.compare(password, admin.password);
-//     if (!isMatch) return res.status(401).json({ message: "Invalid credentials" });
+    const isMatch = await bcrypt.compare(password, admin.password);
+    if (!isMatch) return res.status(401).json({ message: "Invalid credentials" });
 
-//     const token = jwt.sign(
-//       { id: admin.id, email: admin.email, role: admin.role },
-//       process.env.ADMIN_ACCESS_SECRET_KEY,
-//       { expiresIn: "2h" }
-//     );
+    const token = jwt.sign(
+      { id: admin.id, email: admin.email, role: admin.role },
+      process.env.ADMIN_ACCESS_SECRET_KEY,
+      { expiresIn: "2h" }
+    );
 
-//     return res.status(200).json({
-//       status: "success",
-//       message: "Admin logged in successfully",
-//       token,
-//       admin: {
-//         id: admin.id,
-//         full_name: admin.full_name,
-//         email: admin.email,
-//         role: admin.role,
-//       },
-//     });
-//   } catch (error) {
-//     res.status(500).json({ message: "Login failed", error: error.message });
-//   }
-// };
+    return res.status(200).json({
+      status: "success",
+      message: "Admin logged in successfully",
+      token,
+      admin: {
+        id: admin.id,
+        full_name: admin.full_name,
+        email: admin.email,
+        role: admin.role,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Login failed", error: error.message });
+  }
+};
 
  
 
@@ -75,17 +75,14 @@
 
 
 
+// export const saveAdmin = async (req,res) => {
 
+// let data = req.body;
 
+// data.password = await bcrypt.hash(data.password, 10);
 
-// // export const saveAdmin = async (req,res) => {
+// const values =Object.values(data) ;
 
-// // let data = req.body;
-
-// // data.password = await bcrypt.hash(data.password, 10);
-
-// // const values =Object.values(data) ;
-
-// // await pool.query('INSERT INTO admins (full_name, email, password, role) VALUES ($1, $2, $3, $4)',values);
-// // }
+// await pool.query('INSERT INTO admins (full_name, email, password, role) VALUES ($1, $2, $3, $4)',values);
+// }
 
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,11 +1,12 @@
 import express, { Router } from "express";
-//import { adminLogin} from "../controller/adminController.js";
+import { adminLogin} from "../controller/adminController.js";
 
 const router = express.Router();
 
-//router.post("/api/admin/login",adminLogin);
+router.post("/api/admin/login",adminLogin);
 //router.get("/api/admin/getUsersByStauts",);
 //router.post("/api/admin/changeStatusByUserId/:id",);
 //router.post("/api/admin/saveadmin",saveAdmin);
 
 export default router;
+
