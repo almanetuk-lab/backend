8f4582bc0e5d0abb50b0efd8354b53961adca26b
almanetuk-lab <almanetuk@gmail.com>
2025-10-29 15:51:18 +0530

    getdetails code update

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,6 +1,6 @@
 // routes/adminRoutes.js
 import express from "express";
-import { adminLogin, approveUser, deactivateUser, getAllUsers, onHoldUser } from "../controller/adminController.js";
+import { adminLogin, approveUser, deactivateUser, getAllUserDetails, getAllUsers, onHoldUser } from "../controller/adminController.js";
 import { verifyAdminToken } from "../middleware/verifyAdminToken.js";
 import { validateAccessToken } from "../middleware/verfiytoken.js";
 
@@ -12,5 +12,6 @@ router.post("/api/admin/approveUser", verifyAdminToken, approveUser);
 router.post("/api/admin/on-hold", verifyAdminToken, onHoldUser);
 router.post("/api/admin/deactivate", verifyAdminToken, deactivateUser);
 router.get("/api/admin/users", validateAccessToken, getAllUsers);
+router.get("/api/admin/getdetails/:id", validateAccessToken, getAllUserDetails);
 
 export default router;
