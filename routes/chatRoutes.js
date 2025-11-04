609a514dc09b9f14794d427b254de9ece99a87ec
almanetuk-lab <almanetuk@gmail.com>
2025-11-04 17:03:00 +0530

    today code

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,21 +1,14 @@
 import express from "express";
-import { 
-  uploadFile, 
-  getAllUsers, 
-  getMessagesForUser, 
-  getAllMessages, 
-  addReaction, 
-  getAllReactions 
-} from "../controller/chatController.js";
+import { uploadFile, getAllUsers, getMessagesForUser, getAllMessages, addReaction, getAllReactions } from "../controller/chatController.js";
 
 const router = express.Router();
 
 // Chat / File / Reaction Routes
 router.post("/api/upload", uploadFile); 
 router.get("/api/users", getAllUsers); //
-router.get("/api/messages/:userId", getMessagesForUser);
-router.post("/api/messages", getAllMessages);//
+router.get("/api/messages/:userId", getMessagesForUser); //
+router.post("/api/messages", getAllMessages); //
 router.post("/api/reactions", addReaction);
-router.get("/api/reactions", getAllReactions);
+router.get("/api/reactions", getAllReactions);//
 
 export default router;
